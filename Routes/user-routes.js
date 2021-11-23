ff87f4ebe0bc29077ffbbe4c2cbbca38443186bb
niharika2k00 <dniharika16@gmail.com>
2021-11-24 02:02:31 +0530

    add + create table1

--- Routes/user-routes.js
+++ Routes/user-routes.js
@@ -7,9 +7,6 @@ import { CreateDatabase, CreateUserTable, AddNewUser } from '../Controllers/user
 router.get('/createDb', CreateDatabase);
 router.get('/createUserTable', CreateUserTable);
 router.get('/addUser', AddNewUser);
-// router.post('/logout', auth, userController.logoutUser);
-// router.post('/edit', upload.single('image'), userController.editUser);
-// router.post('/getExam', userController.getExam);
-// router.post('/registerInExam', userController.registerInExam);
+
 
 export default router;
