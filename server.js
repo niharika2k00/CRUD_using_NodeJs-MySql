e5f9650612fdf81aa2e0aa5e0cfced4807b6602a
niharika2k00 <dniharika16@gmail.com>
2021-11-25 01:47:24 +0530

    user table full done

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
-import { ConnectDB, getDb } from './DBConnection/DB.js';
 import userRoutes from './Routes/user-routes.js';
 
 
@@ -9,40 +8,23 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(express.json());
-// ConnectDB();
 
 // app.get(endpoint, callback)
 app.get('/', (req, res) => {
     res.send('get success !')
 })
 
-// const db = ConnectDB();
-const db = getDb();
-// console.log("db = ", db);
-
-// CREATE DB
-// app.get('/createDb', (req, res) => {
-//     let sql = 'CREATE DATABASE practiseDatabase';
-//     db.query(sql, (err, result) => {
-//         if (err) throw err;
-//         res.send('Database Created :) ');
-//         console.log("Result = ", result);
-//     });
-// });
-
-
 // INSERT DATA ---- POST REQUEST 
 /* app.get('/addpost', (req, res) => {
     let post = { name: 'niharika', title: 'post 1 created' };
-    let aa = 'INSERT INTO Users SET ?';
-    let query = db.query(aa, post, (err, result) => {
+    let sqlCommand = 'INSERT INTO Users SET ?';
+    let query = db.query(sqlCommand, post, (err, result) => {
         if (err) throw err;
         console.log("Result = ", result);
         res.status(200);
         res.send('added ');
     });
 }) */
-
 
 
 // SYNTAX: app.use(path, callback)
